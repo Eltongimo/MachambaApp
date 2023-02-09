b1afee5e9885226c79f265cc71034b5cad3de369
baratabarata <barataestevaomario@gmail.com>
2023-02-09 08:38:22 -0400

    update

--- app/src/main/java/com/example/machambaapp/Activity_Login.java
+++ app/src/main/java/com/example/machambaapp/Activity_Login.java
@@ -2,13 +2,25 @@ package com.example.machambaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Login extends AppCompatActivity {
-
+   private Button buttonLogar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+           buttonLogar=(Button) findViewById(R.id.idButtonlogar);
+           buttonLogar.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+                   Intent intent=new Intent(Activity_Login.this, ActivityChooseAction.class);
+                   startActivity(intent);
+               }
+           });
     }
 }
--- app/src/main/java/com/example/machambaapp/Activity_UserRegister.java
+++ app/src/main/java/com/example/machambaapp/Activity_UserRegister.java
@@ -3,12 +3,49 @@ package com.example.machambaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class Activity_UserRegister extends AppCompatActivity {
+
+    String[] itemsDistrito = {"Monapo", "Moma", "Montepues"};
+    String[] itemsPostoAdministrativo = {"p-Monapo", "p-Moma", "p-Montepues"};
+    String[] itemsLocalidade = {"L-Monapo", "L-Moma", "L-Montepues"};
+    String[] itemsComunidade = {"C-Monapo", "C-Moma", "C-Montepues"};
+    AutoCompleteTextView autoCompleteDistrito;
+    AutoCompleteTextView autoCompletePostoAdministrativo;
+    AutoCompleteTextView autoCompleteLocalidade;
+    AutoCompleteTextView autoCompleteComunidade;
+    ArrayAdapter<String> adapterItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_register);
+
+        autoCompleteDistrito = (AutoCompleteTextView) findViewById(R.id.auto_select);
+        autoCompletePostoAdministrativo = (AutoCompleteTextView) findViewById(R.id.idPostoAdministrativo);
+        autoCompleteLocalidade = (AutoCompleteTextView) findViewById(R.id.idLocalidade);
+        autoCompleteComunidade = (AutoCompleteTextView) findViewById(R.id.idComunidade);
+
+
+
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item_distrito, itemsDistrito);
+        autoCompleteDistrito.setAdapter(adapterItems);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item_posto_administrativo, itemsPostoAdministrativo);
+        autoCompletePostoAdministrativo.setAdapter(adapterItems);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item_localidade, itemsLocalidade);
+        autoCompleteLocalidade.setAdapter(adapterItems);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_item_comunidade, itemsComunidade);
+        autoCompleteComunidade.setAdapter(adapterItems);
+
+       // autoCompleteTextView.setOnClickListener(s);
+
+
+
+
+
     }
+
+
 }
